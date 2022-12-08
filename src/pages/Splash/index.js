06805290d3151bb72add74fc069e2262cf9cbfbc
Muhammad Tazakka Ramadhan <108384217+mtazakka@users.noreply.github.com>
2022-12-08 09:41:15 +0700

    Finish FE

---
+++ src/pages/Splash/index.js
@@ -0,0 +1,31 @@
+import {Image, ImageBackground, StyleSheet, Text, View} from 'react-native';
+import React, {useEffect} from 'react';
+import {SplashBackground, Logo} from '../../assets';
+
+const Splash = ({navigation}) => {
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.replace('MainApp');
+    }, 3000);
+  }, [navigation]);
+
+  return (
+    <ImageBackground source={SplashBackground} style={styles.background}>
+      <Image source={Logo} style={styles.logo} />
+    </ImageBackground>
+  );
+};
+
+export default Splash;
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    width: 222,
+    Heigh: 103,
+  },
+});
